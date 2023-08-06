0c9e42f19c19f1196a8c4ae6e8062305a649e5e9
ness727 <72534875+ness727@users.noreply.github.com>
2023-08-06 11:52:28 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.15 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.util.HashSet;
+
+class Solution {
+    public int[] solution(int n, int m) {
+        HashSet<Integer> nSet = new HashSet<>();
+        HashSet<Integer> mSet = new HashSet<>();
+        
+        for (int i = 1; i <= (int)Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                nSet.add(i);
+                nSet.add(n / i);
+            }
+        }
+        
+        for (int i = 1; i <= (int)Math.sqrt(m); i++) {
+            if (m % i == 0) {
+                mSet.add(i);
+                mSet.add(m / i);
+            }
+        }
+        nSet.retainAll(mSet);
+        
+        return new int[] { Collections.max(nSet), n / Collections.max(nSet) * m};
+    }
+}
