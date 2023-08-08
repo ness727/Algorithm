118ac94fbb385175920491a48310569f510853b3
ness727 <72534875+ness727@users.noreply.github.com>
2023-08-08 21:12:04 +0900

    [level 2] Title: 영어 끝말잇기, Time: ., Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        ArrayList<String> wordList = new ArrayList<>();
+        
+        for (int i = 0; i < words.length; i += n) {
+            for (int j = i; j < (i + n > words.length ? words.length : i + n); j++) {
+                if (j != 0) 
+                    if (words[j].charAt(0) != words[j - 1].charAt(words[j - 1].length() - 1))
+                        return new int[] { j % n + 1, i / n + 1 };
+                
+                if (!wordList.contains(words[j])) wordList.add(words[j]);
+                else return new int[] { j % n + 1, (int)Math.floor((double)i / n) + 1 };
+            }
+        }
+        return new int[] { 0, 0 };
+    }
+}
