9ba9863d4c932b174e7216bb640843c52ed637eb
ness727 <72534875+ness727@users.noreply.github.com>
2023-08-11 10:53:22 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = n;
+        
+        while (true) {
+            answer++;
+            if (Integer.bitCount(n) == Integer.bitCount(answer)) return answer;
+        }
+    }
+}
