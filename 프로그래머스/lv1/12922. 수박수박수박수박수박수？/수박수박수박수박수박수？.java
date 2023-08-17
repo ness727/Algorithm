41efe7c4f3b83a156fca7974b3514dc8c7111eed
ness727 <72534875+ness727@users.noreply.github.com>
2023-08-17 10:39:19 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.14 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,5 +1,5 @@
 class Solution {
     public String solution(int n) {
-        return new StringBuilder().append("수박".repeat(n/2)).toString() + ((n % 2 != 0) ? "수" : "");
+        return "수박".repeat(n/2) + ((n % 2 != 0) ? "수" : "");
     }
 }
