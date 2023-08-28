75a047a8c54426cf286bff76e70fdd270b947171
ness727 <72534875+ness727@users.noreply.github.com>
2023-08-28 10:28:27 +0900

    [level 1] Title: 폰켓몬, Time: 26.63 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,11 +1,7 @@
-import java.util.Set;
-import java.util.HashSet;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> numSet = new HashSet<>();
-        
-        for (int num : nums) numSet.add(num);
-        return numSet.size() < (nums.length / 2) ? numSet.size() : nums.length / 2;
+        return (int)Math.min(Arrays.stream(nums).distinct().count(), nums.length / 2);
     }
 }
