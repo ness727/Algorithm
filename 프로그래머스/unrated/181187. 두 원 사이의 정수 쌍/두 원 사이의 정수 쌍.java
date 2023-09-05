796fe4e20cdaaab4f740eae11e5b04b68c3aa44f
ness727 <72534875+ness727@users.noreply.github.com>
2023-09-05 10:08:36 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 14.41 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -1,17 +1,17 @@
 class Solution {
     public long solution(int r1, int r2) {
         long answer = 0;
-        final long R1_POW = (long) r1 * r1;
-        final long R2_POW = (long) r2 * r2;
+        final long r1_pow = (long) r1 * r1;
+        final long r2_pow = (long) r2 * r2;
 
         for (long x = 1; x < r2; x++) {
             if (x < r1) {
-                long r1Y = (long) Math.floor(Math.sqrt(R1_POW - x * x));
-                long r2Y = (long) Math.floor(Math.sqrt(R2_POW - x * x));
+                long r1Y = (long) Math.floor(Math.sqrt(r1_pow - x * x));
+                long r2Y = (long) Math.floor(Math.sqrt(r2_pow - x * x));
                 answer += r2Y - r1Y;
-                if (r1Y * r1Y + x * x == R1_POW) answer++;
+                if (r1Y * r1Y + x * x == r1_pow) answer++;
             }
-            else answer += (long) Math.floor(Math.sqrt(R2_POW - x * x));
+            else answer += (long) Math.floor(Math.sqrt(r2_pow - x * x));
         }
         return (answer + (r2 - r1 + 1)) * 4;
     }
