646e87cef262fdfe5d538df8404644904e1df3e1
ness727 <72534875+ness727@users.noreply.github.com>
2023-12-27 11:33:57 +0900

    [level unrated] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.02 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/unrated/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/unrated/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -3,18 +3,14 @@ class Solution {
         int answer = 0;
         String myColor = board[h][w];
         
-        if (h - 1 >= 0) {  // 상
-            if (myColor.equals(board[h - 1][w])) answer++;
-        }
-        if (h + 1 < board.length) {  // 하
-            if (myColor.equals(board[h + 1][w])) answer++;
-        }
-        if (w - 1 >= 0) {  // 좌
-            if (myColor.equals(board[h][w - 1])) answer++;
-        }
-        if (w + 1 < board[0].length) {  // 우
-            if (myColor.equals(board[h][w + 1])) answer++;
-        }
+        // 상
+        if (h - 1 >= 0 && myColor.equals(board[h - 1][w])) answer++;
+        // 하
+        if (h + 1 < board.length && myColor.equals(board[h + 1][w])) answer++;
+        // 좌
+        if (w - 1 >= 0 && myColor.equals(board[h][w - 1])) answer++;
+        // 우
+        if (w + 1 < board[0].length && myColor.equals(board[h][w + 1])) answer++;
         
         return answer;
     }
