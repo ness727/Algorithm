bd5fbed8af9aa85ab944d6476e5408316709af32
ness727 <72534875+ness727@users.noreply.github.com>
2024-08-08 18:00:13 +0900

    [level 2] Title: 디펜스 게임, Time: 92.45 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/README.md
+++ 프로그래머스/2/142085. 디펜스 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] 디펜스 게임 - 142085 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085) 
 
 ### 성능 요약
 
-메모리: 164 MB, 시간: 73.82 ms
+메모리: 149 MB, 시간: 92.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 08일 17:57:58
+2024년 08월 08일 18:00:10
 
 ### 문제 설명
 
--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -3,20 +3,20 @@ import java.util.Collections;
 
 class Solution {
     public int solution(int n, int k, int[] enemy) {
-        PriorityQueue<Integer> preEnemyQueue 
+        PriorityQueue<Integer> enemyQueue 
             = new PriorityQueue<>(Collections.reverseOrder());
         int answer = 0;
         
         for (int i = 0; i < enemy.length; i++) {
             if (enemy[i] <= n) {
-                preEnemyQueue.add(enemy[i]);
+                enemyQueue.add(enemy[i]);
                 n -= enemy[i];
             } else {
                 if (0 < k) {
-                    if (!preEnemyQueue.isEmpty() 
-                        && enemy[i] <= preEnemyQueue.peek()) {
-                        n += (preEnemyQueue.poll() - enemy[i]);
-                        preEnemyQueue.add(enemy[i]);
+                    if (!enemyQueue.isEmpty() 
+                        && enemy[i] <= enemyQueue.peek()) {
+                        n += (enemyQueue.poll() - enemy[i]);
+                        enemyQueue.add(enemy[i]);
                     }
                     k--;
                 } else break;
