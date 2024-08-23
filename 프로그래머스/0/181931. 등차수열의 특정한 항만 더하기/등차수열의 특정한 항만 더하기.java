9f0f598d39650a9cb5587ed6b6dbb34dbb933066
ness727 <72534875+ness727@users.noreply.github.com>
2024-08-23 15:20:49 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 6.62 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        return IntStream.range(0, included.length)
+            .map(i -> included[i] ? a + d * i : 0)
+            .sum();
+    }
+}
