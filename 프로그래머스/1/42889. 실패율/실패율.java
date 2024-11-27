54dabfa31db8a1be6c36bd783a023408f694f016
ness727 <72534875+ness727@users.noreply.github.com>
2024-11-27 14:01:53 +0900

    [level 1] Title: 실패율, Time: 11.51 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,42 +1,27 @@
-import java.util.Arrays;
-import java.util.stream.IntStream;
-import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] answer = {};
+        int[] stageCount = new int[N + 2];
+        int total = stages.length;
+        HashMap<Integer, Float> failureMap = new HashMap<>();
         
-        // 실패율 저장
-        float[] failures = new float[N];
-        // 초기 전체 인원
-        int playerCnt = stages.length;
-        
-        // 유저 스테이지 (stages) 오름차순
-        Arrays.sort(stages);
-        
-        // 스테이지별 남아있는 인원 구하기
-        // 현재 스테이지 실패율 (머물러있는 인원 / 도달한 인원)
-        // 다음 스테이지부터 도달한 인원 -= 이전 스테이지 머물러 있는 인원
-        int stage = 1;
-        for (int i = 0; i < stages.length && stage <= N;) {
-            // 현재 스테이지 머물러 있는 인원 구하기
-            int curStagePlayer = 0;
-            while (i < stages.length && stages[i] == stage) {
-                curStagePlayer++;
-                i++;
-            }
-            
-            // 실패율 계산
-            failures[stage - 1] = (float)curStagePlayer / playerCnt;
-            playerCnt -= curStagePlayer;
-            stage++;
+        for (int stage : stages) {
+            stageCount[stage]++;
         }
         
-        // 실패율 내림차순 & 같으면 스테이지 번호로 오름차순
-        return IntStream.range(0, N)
-            .boxed()
-            .sorted((s1, s2) -> Float.compare(failures[s2], failures[s1]))
-            .mapToInt(i -> i + 1)
+        for (int i = 1; i <= N; i++) {
+            if (stageCount[i] == 0) failureMap.put(i, 0f);
+            else {
+                failureMap.put(i, (float)stageCount[i] / total);
+                total -= stageCount[i];
+            }
+        }
+        
+        return failureMap.entrySet().stream()
+            .sorted((e1, e2) -> Float.compare(e2.getValue(), e1.getValue()))
+            .mapToInt(Map.Entry::getKey)
             .toArray();
     }
 }
